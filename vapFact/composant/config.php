96febac6a23c2ff3a5efb040dac0280fe32a926d
unknown <stevegreiner68@gmail.com>
2022-03-30 06:45:52 +0400

    maj config.php

--- vapFact/composant/config.php
+++ vapFact/composant/config.php
@@ -1,39 +1,39 @@
 <?php                                  
-    // $host = "localhost";             
-    // $bdd_name = "vap_factory";       
-    // $indentifiant = "root";           
-    // $password = "";     
+// $host = "localhost";             
+// $bdd_name = "vap_factory";       
+// $indentifiant = "root";           
+// $password = "";     
     
-
-//###---developpent---############################
-$host_dev = "localhost";             
-$bdd_name_dev = "sprint-06"; 
-$indentifiant_dev = "sprint_06";           
-$password_dev = "sprint_06"; 
-//###---production---############################
-$host_prod = "109.234.164.161";             
-$bdd_name_prod = "sc1lgvu9627_greiner-steve.sprint-06"; 
-$indentifiant_prod = $_SERVER['DB_USER'];           
-$password_prod = $_SERVER['DB_PASSWORD']; 
+// try {
+//     $bdd = new PDO(
+//             "mysql:host=$host; dbname=$bdd_name; charset=utf8", 
+//             $indentifiant, 
+//             $password);
+//     $bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// } catch (Exception $e) {
+//     die('Erreur : ' . $e -> getMessage());
+// }
 
 try {
-    if (strcmp($_SERVER['ENVIRONNEMENT_TYPE'], "developement") == 0) {
+    // dev configuration
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "development") == 0) {
         $bdd = new PDO(
-            "mysql:host=$host_dev; dbname=$bdd_name_dev; charset=utf8", 
-            $indentifiant_dev, 
-            $password_dev);
-        $bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            'mysql:host=localhost;dbname=vap_factory;charset=utf8',
+            'root',
+            ''
+        );
     }
 
-    if (strcmp($_SERVER['ENVIRONNEMENT_TYPE'], "production") == 0) {
+    // prod configuration
+    if (strcmp($_SERVER['ENVIRONMENT_TYPE'], "production") == 0) {
         $bdd = new PDO(
-            "mysql:host=$host_prod; dbname=$bdd_name_prod; charset=utf8", 
-            $indentifiant_prod, 
-            $password_prod);
-        $bdd -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            'mysql:host=109.234.164.161;dbname=sc1lgvu9627_rozar-fabien.sprint-06;charset=utf8',
+            $_SERVER['DB_USER'],
+            $_SERVER['DB_PASSWORD']
+        );
     }
 } catch (Exception $e) {
-    die('Erreur : ' . $e -> getMessage());
+    die('Erreur : ' . $e->getMessage());
 }
 
-?>
+?>3
