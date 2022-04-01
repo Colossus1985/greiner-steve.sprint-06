b42d56ecdb28d014c41e7ec28687a126b44f4c26
unknown <stevegreiner68@gmail.com>
2022-04-01 13:29:19 +0400

    correction get new price and new stock

--- vapFact/exec/modifProdExec.php
+++ vapFact/exec/modifProdExec.php
@@ -9,58 +9,65 @@ if (isset($_POST['submit_modif_prod'])) {
     if (!empty($_POST['new_window'])) {
         $new_discription = test_input_stock_detail($_POST['new_window']);} else {$new_discription = $_POST['old_window'];}
         
-    if (isset($_POST['new_stock']) && $_POST['new_stock'] == 0) {
-        $new_stock = 0;
-    } else {
-        $new_stock = $_POST['new_stock'];
-    } 
-    if (!isset($_POST['new_stock']) && $_POST['old_stock'] == 0) {
-            $new_stock = 0;
-    } else {
-        $new_stock = $_POST['old_stock'] ;
-    }
 
-    if (isset($_POST['new_price_buy']) && $_POST['new_price_buy'] == 0) {
-        $new_price_buy = 0;
-    } else {
-        $new_price_buy = number_format($_POST['new_price_buy'],2);
-    } 
-    if (!isset($_POST['new_price_buy']) && $_POST['old_price_buy'] == 0) {
-            $new_price_buy = 0;
-    } else {
-        $new_price_buy = $_POST['old_price_buy'] ;
-    }
 
-    if (isset($_POST['new_price_sell']) && $_POST['new_price_sell'] == 0) {
-        $new_price_sell = 0;
-    } else {
-        $new_price_sell = number_format($_POST['new_price_sell'],2);
-    } 
-    if (!isset($_POST['new_price_sell']) && $_POST['old_price_sell'] == 0) {
-            $new_price_sell = 0;
-    } else {
-        $new_price_sell = $_POST['old_price_sell'] ;
-    }
+
+        if (!empty($_POST['new_stock'])) {
+            echo "new_stock setted<br>";
+            if (!empty($_POST['new_stock']) && $_POST['new_stock'] == 0) {
+                echo "new_stock setted 0<br>";
+                $new_stock = 0;
+            } else {
+                echo "new_stock setted 1<br>";
+                $new_stock = $_POST['new_stock'];
+            } 
+        } elseif (empty($_POST['new_stock']) && $_POST['old_stock'] == 0) {
+                echo "new_stock not setted old_stock 0<br>";
+                $new_stock = 0;
+        } else {
+            echo "new_stock not setted old_stock 1<br>";
+            $new_stock = $_POST['old_stock'] ;
+        }
+    
+        if (!empty($_POST['new_price_buy'])) {
+            if ($_POST['new_price_buy'] == 0) {
+                $price = (int)$_POST['new_price_buy'];
+                $new_price_buy = number_format($price,2);
+            } else {
+             $new_price_buy = number_format($_POST['new_price_buy'],2);
+            }
+        }
+    
+        if (!empty($_POST['new_price_sell'])) {
+            if ($_POST['new_price_sell'] == 0) {
+                $price = (int)$_POST['new_price_sell'];
+                $new_price_sell = number_format($price,2);
+            } else {
+            $new_price_sell = number_format($_POST['new_price_sell'],2);
+            }
+        }
+
+
 
     if (isset($_POST['new_type'])) {
         $new_type = $_POST['new_type'];} else {$new_type = $_POST['old_type'];}
 
-    // echo "ref reference: " . $ref . " [" .$_POST['old_ref']."]<br>";
-    // echo "ref reference: " . $ref . " [" .$_POST['new_ref']."]<br>";
-    // echo "new_ref: " . $new_ref ." [".$_POST['old_ref']."]<br>";
-    // echo "new_ref: " . $new_ref ." [".$_POST['new_ref']."]<br>";
-    // echo "new_name: " . $new_name ." [" .$_POST['old_name']."]<br>";
-    // echo "new_name: " . $new_name ." [" .$_POST['new_name']."]<br>";
-    // echo "new_discription: " . $new_discription ." [" .$_POST['old_window']."]<br>";
-    // echo "new_discription: " . $new_discription ." [" .$_POST['new_window']."]<br>";
-    // echo "1new_stock: " . $new_stock ." [".$_POST['old_stock']."]<br>";
-    // echo "2new_stock: " . $new_stock ." [".$_POST['new_stock']."]<br>";
-    // echo "1new_price_buy: " . $new_price_buy ." [".$_POST['old_price_buy']."]<br>";
-    // echo "2new_price_buy: " . $new_price_buy ." [".$_POST['new_price_buy']."]<br>";
-    // echo "1new_price_sell: " . $new_price_sell ." [".$_POST['old_price_sell']."]<br>";
-    // echo "2new_price_sell: " . $new_price_sell ." [".$_POST['new_price_sell']."]<br>";
-    // echo "new_type: " . $new_type ." [".$_POST['old_type']."]<br>";
-    // echo "new_type: " . $new_type ." [".$_POST['new_type']."]<br>";
+    // echo "1ref reference: " . $ref . " [" .$_POST['old_ref']."]<br>";
+    // echo "2ref reference: " . $ref . " [" .$_POST['new_ref']."]<br>";
+    // echo "1new_ref: " . $new_ref ." [".$_POST['old_ref']."]<br>";
+    // echo "2new_ref: " . $new_ref ." [".$_POST['new_ref']."]<br>";
+    // echo "1new_name: " . $new_name ." [" .$_POST['old_name']."]<br>";
+    // echo "2new_name: " . $new_name ." [" .$_POST['new_name']."]<br>";
+    // // echo "1new_discription: " . $new_discription ." [" .$_POST['old_window']."]<br>";
+    // echo "2new_discription: " . $new_discription ." [" .$_POST['new_window']."]<br>";
+    echo "1new_stock: " . $new_stock ." [".$_POST['old_stock']."]<br>";
+    echo "2new_stock: " . $new_stock ." [".$_POST['new_stock']."]<br>";
+    echo "1new_price_buy: " . $new_price_buy ." [".$_POST['old_price_buy']."]<br>";
+    echo "2new_price_buy: " . $new_price_buy ." [".$_POST['new_price_buy']."]<br>";
+    echo "1new_price_sell: " . $new_price_sell ." [".$_POST['old_price_sell']."]<br>";
+    echo "2new_price_sell: " . $new_price_sell ." [".$_POST['new_price_sell']."]<br>";
+    // echo "1new_type: " . $new_type ." [".$_POST['old_type']."]<br>";
+    // echo "2new_type: " . $new_type ." [".$_POST['new_type']."]<br>";
 
     include 'composant/config.php';
 
